f44d7b9b71da6f46acf385d5e6c21dd69ba858de
Mani Bharathi <manibharathi93617@gmail.com>
2025-01-20 21:49:34 +0530

    Johnson_Counter_tb.v

---
+++ Day_27/Johnson_Counter_tb.v
@@ -0,0 +1,25 @@
+module johnson_counter_tb;
+    reg clk;
+    reg rst_n;
+    wire [3:0] q;
+    johnson_counter uut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .q(q)
+    );
+    always
+    begin
+        #5 clk = ~clk;
+    end
+    initial
+    begin
+        clk = 0;
+        rst_n=0;
+        #10 rst_n = 1;
+        #100;$finish;
+    end
+    initial
+    begin
+        $monitor($time," Reset = %b, q = %b", rst_n, q);
+    end
+endmodule
