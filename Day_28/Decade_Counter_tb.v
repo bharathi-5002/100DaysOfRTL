ff9def12c7966e8e158582d6c670b035fa297a6b
Mani Bharathi <manibharathi93617@gmail.com>
2025-01-20 21:37:57 +0530

    Create Decade_Counter_tb.v

---
+++ Day_28/Decade_Counter_tb.v
@@ -0,0 +1,25 @@
+module decade_counter_tb;
+    reg clk;
+    reg rst_n;
+    wire [3:0] q;
+    decade_counter uut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .q(q)
+    );
+    always begin
+        #5 clk = ~clk;
+    end
+    initial
+    begin
+        clk = 0;
+        rst_n = 0;
+        #10 rst_n = 1;
+        #200;
+        $finish;
+    end
+    initial
+    begin
+        $monitor($time," Reset = %b, q = %b", rst_n, q);
+    end
+endmodule
